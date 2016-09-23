6f15faefcf0e55677ff62ba81338b174187db851
madhephaestus <mad.hephaestus@gmail.com>
2016-09-23 15:33:54 -0400

    cutout got head

--- wearableAnimatronics.groovy
+++ wearableAnimatronics.groovy
@@ -3,27 +3,42 @@ import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 
 LengthParameter thickness 		= new LengthParameter("Material Thickness",3.15,[10,1])
 LengthParameter headDiameter 		= new LengthParameter("Head Dimeter",100,[200,50])
+LengthParameter upperHeadDiam 	= new LengthParameter("Upper Head Height",20,[300,0])
 LengthParameter snoutLen 		= new LengthParameter("Snout Length",63,[200,50])
-LengthParameter jawHeight 		= new LengthParameter("Jaw Height",32,[200,10])
+LengthParameter jawHeight 		= new LengthParameter("Jaw Height",85,[200,10])
 LengthParameter leyeDiam 		= new LengthParameter("Left Eye Diameter",35,[headDiameter.getMM()/2,29])
 LengthParameter reyeDiam 		= new LengthParameter("Right Eye Diameter",35,[headDiameter.getMM()/2,29])
 LengthParameter eyeCenter 		= new LengthParameter("Eye Center Distance",headDiameter.getMM()/2,[headDiameter.getMM(),headDiameter.getMM()/2])
 StringParameter servoSizeParam 			= new StringParameter("hobbyServo Default","towerProMG91",Vitamins.listVitaminSizes("hobbyServo"))
 StringParameter boltSizeParam 			= new StringParameter("Bolt Size","M3",Vitamins.listVitaminSizes("capScrew"))
 
-headDiameter.setMM(180)
-snoutLen.setMM(150)
-eyeCenter.setMM(100)
-leyeDiam.setMM(60)
-reyeDiam.setMM(60)
+servoSizeParam.setStrValue("hv6214mg")
+headDiameter.setMM(400)
+snoutLen.setMM(300)
+eyeCenter.setMM(250)
+leyeDiam.setMM(120)
+reyeDiam.setMM(120)
+jawHeight.setMM(85)
+upperHeadDiam.setMM(50)
 
-//def headParts  = (ArrayList<CSG> )ScriptingEngine.gitScriptRun("https://gist.github.com/e67b5f75f23c134af5d5054106e3ec40.git", "AnimatronicHead.groovy" ,  null )
-
-return ScriptingEngine.gitScriptRun("https://github.com/madhephaestus/Halloween2016.git", "KevinHarringtonScan.stl" ,  null )
+def headParts  = (ArrayList<CSG> )ScriptingEngine.gitScriptRun("https://gist.github.com/e67b5f75f23c134af5d5054106e3ec40.git", "AnimatronicHead.groovy" ,  null )
+println "Loading head"
+CSG scannedHead =  ScriptingEngine.gitScriptRun("https://github.com/madhephaestus/Halloween2016.git", "KevinHarringtonScan.stl" ,  null )
 		.movey(-400)
 		.movex(-60)
 		.rotx(-25)
 		.rotz(185)
+		.movez(-65)
+		.movex(75)
+println "Making cutout"
+CSG cutout = scannedHead
+			.hull()
+			.scale(1.05)
+BowlerStudioController.addCsg(cutout)		
+println "Performing cutout"
+def retParts  = headParts.collect{
+	it.difference(cutout)
+}
 
-//headParts.add(scannedHead)
-//return headParts
+retParts.add(scannedHead)
+return retParts
